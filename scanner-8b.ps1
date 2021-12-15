513f86eaf952869a3ec48116663c7db04f00c302
Stephen Nix <sn@nix.uno>
2021-12-15 07:48:24 -0500

    Fix formatting

--- scanner-8b.ps1
+++ scanner-8b.ps1
@@ -16,23 +16,25 @@ param (
     [Parameter(Mandatory=$false)][char]$usrMitigate = $env:usrMitigate
 )
 
+$scriptObject = Get-Item -Path $script:PSCommandPath
+$script:workingPath = $($scriptObject.DirectoryName)
 [string]$varch=[intPtr]::Size*8
 $script:varDetection=0
 $varEpoch=[int][double]::Parse((Get-Date -UFormat %s))
 
-write-host "Log4j/Log4Shell CVE-2021-44228 Scanning/Mitigation Tool (seagull/Datto)"
-write-host "======================================================================="
+Write-Host "Log4j/Log4Shell CVE-2021-44228 Scanning/Mitigation Tool (seagull/Datto)"
+Write-Host "======================================================================="
 if ($env:CS_CC_HOST) {
-    write-host "Set up a File/Folder Size Monitor against devices"
-    write-host "(File/s named $env:PROGRAMDATA\CentraStage\L4Jdetections.txt : is over : 0MB)"
-    write-host "to alert proactively if this Component reports signs of infection."
-    write-host "======================================================================="
+    Write-Host "Set up a File/Folder Size Monitor against devices"
+    Write-Host "(File/s named $env:PROGRAMDATA\CentraStage\L4Jdetections.txt : is over : 0MB)"
+    Write-Host "to alert proactively if this Component reports signs of infection."
+    Write-Host "======================================================================="
 }
 
 #is there already a detections.txt file?
-if (test-path "$env:PROGRAMDATA\CentraStage\L4Jdetections.txt" -ErrorAction SilentlyContinue) {
-    write-host "- An existing L4JDetections.txt file was found. It has been renamed to:"
-    write-host "  $varEpoch-L4JDetections.txt"
+if (Test-Path "$env:PROGRAMDATA\CentraStage\L4Jdetections.txt" -ErrorAction SilentlyContinue) {
+    Write-Host "- An existing L4JDetections.txt file was found. It has been renamed to:"
+    Write-Host "  $varEpoch-L4JDetections.txt"
     Rename-Item -Path "$env:PROGRAMDATA\CentraStage\L4Jdetections.txt" "$env:PROGRAMDATA\CentraStage\$varEpoch-L4Jdetections.txt" -Force
 }
 
@@ -40,37 +42,37 @@ if (test-path "$env:PROGRAMDATA\CentraStage\L4Jdetections.txt" -ErrorAction Sile
 switch ($usrMitigate) {
     'Y' {
         if ([System.Environment]::GetEnvironmentVariable('LOG4J_FORMAT_MSG_NO_LOOKUPS','machine') -eq 'true') {
-            write-host "- Log4j 2.10+ exploit mitigation (LOG4J_FORMAT_MSG_NO_LOOKUPS) already set."
+            Write-Host "- Log4j 2.10+ exploit mitigation (LOG4J_FORMAT_MSG_NO_LOOKUPS) already set."
         } else {
-            write-host "- Enabling Log4j 2.10+ exploit mitigation: Enable LOG4J_FORMAT_MSG_NO_LOOKUPS"
+            Write-Host "- Enabling Log4j 2.10+ exploit mitigation: Enable LOG4J_FORMAT_MSG_NO_LOOKUPS"
             [Environment]::SetEnvironmentVariable("LOG4J_FORMAT_MSG_NO_LOOKUPS","true","Machine")
         }
     } 'N' {
-        write-host "- Reversing Log4j 2.10+ explot mitigation (enable LOG4J_FORMAT_MSG_NO_LOOKUPS)"
-        write-host "  (NOTE: This potentially makes a secure system vulnerable again! Use with caution!)"
+        Write-Host "- Reversing Log4j 2.10+ explot mitigation (enable LOG4J_FORMAT_MSG_NO_LOOKUPS)"
+        Write-Host "  (NOTE: This potentially makes a secure system vulnerable again! Use with caution!)"
         [Environment]::SetEnvironmentVariable("LOG4J_FORMAT_MSG_NO_LOOKUPS","false","Machine")
     } 'X' {
-        write-host "- Not adjusting existing LOG4J_FORMAT_MSG_NO_LOOKUPS setting."
+        Write-Host "- Not adjusting existing LOG4J_FORMAT_MSG_NO_LOOKUPS setting."
     }
 }
 
 #map input variable usrScanScope to an actual value
 switch ($usrScanScope) {
     1   {
-        write-host "- Scan scope: Home Drive"
-        $script:varDrives=@($env:HomeDrive)
+        Write-Host "- Scan scope: Home Drive"
+        $script:varDrives = @($env:HomeDrive)
     } 2 {
-        write-host "- Scan scope: Fixed & Removable Drives"
-        $script:varDrives=Get-WmiObject -Class Win32_logicaldisk | ? {$_.DriveType -eq 2 -or $_.DriveType -eq 3} | ? {$_.FreeSpace} | % {$_.DeviceID}
+        Write-Host "- Scan scope: Fixed & Removable Drives"
+        $script:varDrives = Get-WmiObject -Class Win32_logicaldisk | Where-Object {$_.DriveType -eq 2 -or $_.DriveType -eq 3} | Where-Object {$_.FreeSpace} | ForEach-Object {$_.DeviceID}
     } 3 {
-        write-host "- Scan scope: All drives, including Network"
-        $script:varDrives=Get-WmiObject -Class Win32_logicaldisk | ? {$_.FreeSpace} | % {$_.DeviceID}
+        Write-Host "- Scan scope: All drives, including Network"
+        $script:varDrives = Get-WmiObject -Class Win32_logicaldisk | Where-Object {$_.FreeSpace} | ForEach-Object {$_.DeviceID}
     } default {
-        write-host "! ERROR: Unable to map scan scope variable to a value. (This should never happen!)"
-        write-host "  The acceptable values for env:usrScanScope are:"
-        write-host "    1: Scan files on Home Drive"
-        write-host "    2: Scan files on fixed and removable drives"
-        write-host "    3: Scan files on all detected drives, even network drives"
+        Write-Host "! ERROR: Unable to map scan scope variable to a value. (This should never happen!)"
+        Write-Host "  The acceptable values for env:usrScanScope are:"
+        Write-Host "    1: Scan files on Home Drive"
+        Write-Host "    2: Scan files on fixed and removable drives"
+        Write-Host "    3: Scan files on all detected drives, even network drives"
         exit 1
     }
 }
@@ -78,37 +80,37 @@ switch ($usrScanScope) {
 #if user opted to update yara rules, do that
 if ($usrUpdateDefs) {
     [Net.ServicePointManager]::SecurityProtocol = [Enum]::ToObject([Net.SecurityProtocolType], 3072)
-    $varYaraNew=(new-object System.Net.WebClient).DownloadString('https://github.com/Neo23x0/signature-base/raw/master/yara/expl_log4j_cve_2021_44228.yar')
+    $varYaraNew = (New-Object System.Net.WebClient).DownloadString('https://github.com/Neo23x0/signature-base/raw/master/yara/expl_log4j_cve_2021_44228.yar')
     #quick verification check
     if ($varYaraNew -match 'TomcatBypass') {
         Set-Content -Value $varYaraNew -Path yara.yar -Force
-        write-host "- New YARA definitions downloaded."
+        Write-Host "- New YARA definitions downloaded."
     } else {
-        write-host "! ERROR: New YARA definition download failed."
-        write-host "  Falling back to built-in definitions."
-        copy-item -Path expl_log4j_cve_2021_44228.yar -Destination yara.yar -Force
+        Write-Host "! ERROR: New YARA definition download failed."
+        Write-Host "  Falling back to built-in definitions."
+        Copy-Item -Path expl_log4j_cve_2021_44228.yar -Destination yara.yar -Force
     }
 } else {
-    copy-item -Path expl_log4j_cve_2021_44228.yar -Destination yara.yar -Force
-    write-host "- Not downloading new YARA definitions."
+    Copy-Item -Path expl_log4j_cve_2021_44228.yar -Destination yara.yar -Force
+    Write-Host "- Not downloading new YARA definitions."
 }
 
 #check yara32 and yara64 are there and that they'll run
 foreach ($iteration in ('yara32.exe','yara64.exe')) {
-    if (!(test-path $iteration)) {
-        write-host "! ERROR: $iteration not found. It needs to be in the same directory as the script."
-        write-host "  Download Yara from https://github.com/virustotal/yara/releases/latest and place them here."
+    if (!(Test-Path $iteration)) {
+        Write-Host "! ERROR: $iteration not found. It needs to be in the same directory as the script."
+        Write-Host "  Download Yara from https://github.com/virustotal/yara/releases/latest and place them here."
         exit 1
     } else {
-        write-host "- Verified presence of $iteration."
+        Write-Host "- Verified presence of $iteration."
     }
 
     cmd /c "$iteration -v >nul 2>&1"
     if ($LASTEXITCODE -ne 0) {
-        write-host "! ERROR: YARA was unable to run on this device."
-        write-host "  The Visual C++ Redistributable is required in order to use YARA."
+        Write-Host "! ERROR: YARA was unable to run on this device."
+        Write-Host "  The Visual C++ Redistributable is required in order to use YARA."
         if ($env:CS_CC_HOST) {
-            write-host "  An installer Component is available from the ComStore."
+            Write-Host "  An installer Component is available from the ComStore."
         }
         exit 1
     }
@@ -116,7 +118,7 @@ foreach ($iteration in ('yara32.exe','yara64.exe')) {
 
 #start a logfile
 $host.ui.WriteErrorLine("`r`nPlease expect some permissions errors as some locations are forbidden from traversal.`r`n=====================================================`r`n")
-set-content -Path "log.txt" -Force -Value "Files scanned:"
+Set-Content -Path "log.txt" -Force -Value "Files scanned:"
 Add-Content "log.txt" -Value "====================================================="
 Add-Content "log.txt" -Value " :: Scan Started: $(get-date) ::"
 
@@ -124,28 +126,28 @@ Add-Content "log.txt" -Value " :: Scan Started: $(get-date) ::"
 #get a list of all files-of-interest on the device (depending on scope) :: GCI is broken; permissions errors when traversing root dirs cause aborts (!!!)
 $arrFiles=@()
 foreach ($drive in $varDrives) {
-    gci "$drive\" -force | ? {$_.PSIsContainer} | % {
-        gci -path "$drive\$_\" -rec -force -include *.jar,*.log,*.txt -ErrorAction 0 | % {
-            $arrFiles+=$_.FullName
+    Get-ChildItem "$drive\" -force | Where-Object {$_.PSIsContainer} | ForEach-Object {
+        Get-ChildItem -path "$drive\$_\" -rec -force -include *.jar,*.log,*.txt -ErrorAction 0 | ForEach-Object {
+            $arrFiles += $_.FullName
         }
     }
 }
 
 #scan i: JARs containing vulnerable Log4j code
-write-host "====================================================="
-write-host "- Scanning for JAR files containing potentially insecure Log4j code..."
-$arrFiles | ? {$_ -match '\.jar$'} | % {
+Write-Host "====================================================="
+Write-Host "- Scanning for JAR files containing potentially insecure Log4j code..."
+$arrFiles | Where-Object {$_ -match '\.jar$'} | ForEach-Object {
     if (select-string -Quiet -Path $_ "JndiLookup.class") {
-        write-host "! ALERT: Potentially vulnerable file at $($_)!"
-        if (!(test-path "$env:PROGRAMDATA\CentraStage\L4Jdetections.txt" -ErrorAction SilentlyContinue)) {set-content -path "$env:PROGRAMDATA\CentraStage\L4Jdetections.txt" -Value "! CAUTION !`r`n$(get-date)"}
+        Write-Host "! ALERT: Potentially vulnerable file at $($_)!"
+        if (!(Test-Path "$env:PROGRAMDATA\CentraStage\L4Jdetections.txt" -ErrorAction SilentlyContinue)) {Set-Content -path "$env:PROGRAMDATA\CentraStage\L4Jdetections.txt" -Value "! CAUTION !`r`n$(Get-Date)"}
         Add-Content "$env:PROGRAMDATA\CentraStage\L4Jdetections.txt" -Value "POTENTIALLY VULNERABLE JAR: $($_)"
         $script:varDetection=1
     }
 }
 
 #scan ii: YARA for logfiles & JARs
-write-host "====================================================="
-write-host "- Scanning LOGs, TXTs and JARs for common attack strings via YARA scan......"
+Write-Host "====================================================="
+Write-Host "- Scanning LOGs, TXTs and JARs for common attack strings via YARA scan......"
 foreach ($file in $arrFiles) {
     if ($file -match 'CentraStage' -or $file -match 'L4Jdetections\.txt') {
         #do nothing -- this isn't a security threat; we're looking at the pathname of the log, not the contents
@@ -159,16 +161,16 @@ foreach ($file in $arrFiles) {
         }
 
         #scan it
-        clear-variable yaResult -ErrorAction SilentlyContinue
-        $yaResult=cmd /c "yara$varch.exe `"yara.yar`" `"$file`" -s"
+        Clear-Variable yaResult -ErrorAction SilentlyContinue
+        $yaResult = cmd /c "yara$varch.exe `"yara.yar`" `"$file`" -s"
         if ($yaResult) {
             #sound an alarm
-            write-host "====================================================="
+            Write-Host "====================================================="
             $script:varDetection=1
-            write-host "! DETECTION:"
-            write-host $yaResult
+            Write-Host "! DETECTION:"
+            Write-Host $yaResult
             #write to a file
-            if (!(test-path "$env:PROGRAMDATA\CentraStage\L4Jdetections.txt" -ErrorAction SilentlyContinue)) {set-content -path "$env:PROGRAMDATA\CentraStage\L4Jdetections.txt" -Value "! INFECTION DETECTION !`r`n$(get-date)"}
+            if (!(Test-Path "$env:PROGRAMDATA\CentraStage\L4Jdetections.txt" -ErrorAction SilentlyContinue)) {Set-Content -path "$env:PROGRAMDATA\CentraStage\L4Jdetections.txt" -Value "! INFECTION DETECTION !`r`n$(get-date)"}
             Add-Content "$env:PROGRAMDATA\CentraStage\L4Jdetections.txt" -Value $yaResult
         }
     }
@@ -177,15 +179,15 @@ foreach ($file in $arrFiles) {
 Add-Content "log.txt" -Value " :: Scan Finished: $(get-date) ::"
 
 if ($script:varDetection -eq 1) {
-    write-host "====================================================="
-    write-host "! Evidence of one or more Log4Shell attack attempts has been found on the system."
-    write-host "  The location of the files demonstrating this are noted in the following log:"
-    write-host "  $env:PROGRAMDATA\CentraStage\L4Jdetections.txt"
+    Write-Host "====================================================="
+    Write-Host "! Evidence of one or more Log4Shell attack attempts has been found on the system."
+    Write-Host "  The location of the files demonstrating this are noted in the following log:"
+    Write-Host "  $env:PROGRAMDATA\CentraStage\L4Jdetections.txt"
 } else {
-    write-host "- There is no indication that this system has received Log4Shell attack attempts ."
+    Write-Host "- There is no indication that this system has received Log4Shell attack attempts ."
 }
 
-write-host `r
-write-host "Datto recommends that you follow best practices with your systems by implementing WAF rules,"
-write-host "mitigation and remediation recommendations from your vendors. For more information on Datto's"
-write-host "response to the log4j vulnerabilty, please refer to https://www.datto.com/blog/dattos-response-to-log4shell."
+Write-Host `r
+Write-Host "Datto recommends that you follow best practices with your systems by implementing WAF rules,"
+Write-Host "mitigation and remediation recommendations from your vendors. For more information on Datto's"
+Write-Host "response to the log4j vulnerabilty, please refer to https://www.datto.com/blog/dattos-response-to-log4shell."
